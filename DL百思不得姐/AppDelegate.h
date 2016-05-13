0e1d828fc0c2cb762bb81d51675a30ef0f50c7cb
Daniel Law <Daniel@luobinghengdeMacBook-Pro.local>
2016-05-13 22:45:25 +0800

    Initial Commit

---
+++ DL百思不得姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DL百思不得姐
+//
+//  Created by 罗炳恒 on 16/5/13.
+//  Copyright © 2016年 orgName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DL百思不得姐/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DL百思不得姐
+//
+//  Created by 罗炳恒 on 16/5/13.
+//  Copyright © 2016年 orgName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
